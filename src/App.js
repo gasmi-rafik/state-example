c88b6cb7670855c42e677d7a30bf77d618004d40
rafik gasmi <gasmi.rafik.rg@gmail.com>
2021-02-12 15:28:05 +0100

    state exapmle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react'
 
-function App() {
+// export default class App extends Component {
+// state ={
+//   count: 0 ,
+//   name:"rafik"
+// }
+// increment = () =>
+//  {
+//   this.setState({count :this.state.count += 1})
+//  }
+
+//  decrement = () =>{
+
+//   this.setState({count :this.state.count -= 1} )
+ 
+//  }
+//  handleChange =(e)=>{
+//    this.setState({name:e.target.value})
+//  }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.increment} > +</button>
+//         <span>{this.state.count}</span>
+
+//         <button onClick={this.decrement}>-</button>
+
+//         <br/>
+
+//         <input type="text" placeholder="enter your name " onChange={this.handleChange}/> <span>{this.state.name}</span>
+        
+//       </div>
+//     )
+//   }
+// }
+
+
+//***********************RCC TIMER ******************* */
+
+// import React, { Component } from 'react'
+
+// export default class App extends Component {
+//   state={
+//     time: 0 ,
+//   }
+
+//   tickHandler = ()=>{
+//    this.setState({
+//      time:this.state.time + 1
+//    })
+//    return this.state.time ;
+//   }
+//   componentDidMount(){
+//     var timer = setInterval(this.tickHandler , 1000)
+//     this.setState({
+//       time:timer
+//     })
+//   }
+
+
+//   render() {
+//     const displaytime =()=> {
+
+//       if (this.state.time < 60 ){
+//         return this.state.time +'Second(s)'
+//       }
+//       {
+//         const SS =this.state.time % 60 ;
+//         const MM = ((this.state.time / 60) % 60).toFixed(0) ;
+//         const HH = ((this.state.time / (60*60))).toFixed(0) ;
+//         return `${HH}H:${MM}M:${SS}`
+//       }
+//     }
+//     return (
+//       <div>
+//        <span > {displaytime()}</span> 
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+
+//****************  RFC   *********** */
+// import React ,{useState} from 'react'
+
+// export default function App() {
+
+//   const [count , setCount] = useState(0)
+//   const increment =()=>{
+ 
+//   return setCount( count + 1) 
+//   }
+
+//   const decrement =()=>{
+//     return (setCount( count - 1))
+//   }
+//   return (
+//     <div>
+//       <button onClick={()=>increment()}>+</button>
+//       <span>{count}</span>
+//       <button onClick={()=>decrement()}>-</button>
+//     </div>
+//   )
+// }
+
+//************************************************ */
+
+import React from 'react'
+
+export default function App() {
+
+  var count = 0
+
+  const increment =()=>{
+    count = count + 1 ;
+    console.log(count)
+    return count 
+
+  }
+
+  const decrement =()=>{
+    count = count - 1 ;
+    console.log(count)
+    return count 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <button onClick={()=>increment()}>+</button> 
+     <span>{count}</span>
+     {console.log(count)}
+     <button onClick={()=>decrement()}>-</button>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
